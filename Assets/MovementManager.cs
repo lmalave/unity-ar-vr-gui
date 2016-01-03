870ee2c8a511c33871aa340d1b4937a1d2417606
lmalave <lmalave@hugeinc.com>
2016-01-02 23:51:01 -0500

    Adding rotation to movement

--- Assets/MovementManager.cs
+++ Assets/MovementManager.cs
@@ -23,12 +23,19 @@ public class MovementManager : MonoBehaviour {
 		Vector3 controllerObjectRotation = controllerObject.transform.eulerAngles;
 		positionTextMesh.text = "Position: " + controllerObjectPosition; 
 		rotationTextMesh.text = "Rotation: " + controllerObjectRotation;
-		Vector3 moveDirection = new Vector3 (0f, 0f, 0.01f);
+		Vector3 moveDirection = new Vector3 (0f, 0f, 0.1f);
 		if (controllerObjectRotation.x > 30f && controllerObjectRotation.x < 330f) {
-			if (controllerObjectRotation.x > 180f) {
+			if (controllerObjectRotation.x < 180f) {
 				moveDirection *= -1f;
 			}
 			objectToMove.transform.Translate (moveDirection);
+		}
+		Vector3 rotateDirection = new Vector3 (0f, 1f, 0f);
+		if (controllerObjectRotation.z > 30f && controllerObjectRotation.z < 330f) {
+			if (controllerObjectRotation.z < 180f) {
+				rotateDirection *= -1f;
+			}
+			objectToMove.transform.Rotate (rotateDirection);
 		}
 	}
 }

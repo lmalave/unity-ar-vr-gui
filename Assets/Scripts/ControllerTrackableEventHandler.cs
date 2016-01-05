23baaa1ca218118a5cc484a5727d736ea8eed0b4
lmalave <lmalave@hugeinc.com>
2016-01-04 23:18:00 -0500

    Changing movement controls

--- Assets/Scripts/ControllerTrackableEventHandler.cs
+++ Assets/Scripts/ControllerTrackableEventHandler.cs
@@ -12,20 +12,23 @@ namespace Vuforia
 	public class ControllerTrackableEventHandler : MonoBehaviour,
 	ITrackableEventHandler
 	{
-		#region PRIVATE_MEMBER_VARIABLES
-		
+		#region PUBLIC_MEMBER_VARIABLES
 		public GameObject controllerObject;
 		public GameObject controllerPositionTextObject;
 		public GameObject controllerRotationTextObject;
 		public GameObject character;
 		public GameObject characterPositionTextObject;
 		public GameObject characterRotationTextObject;
+		public GameObject directionPointer;
+		public GameObject pointerPositionTextObject;
+		#endregion // PUBLIC_MEMBER_VARIABLES
 
+		#region PRIVATE_MEMBER_VARIABLES
 		private TextMesh controllerPositionTextMesh;
 		private TextMesh controllerRotationTextMesh;
 		private TextMesh characterPositionTextMesh;
 		private TextMesh characterRotationTextMesh;
-
+		private TextMesh pointerPositionTextMesh;
 		private TrackableBehaviour mTrackableBehaviour;
 		private bool isTracked = false;
 
@@ -48,6 +51,7 @@ namespace Vuforia
 			controllerRotationTextMesh = controllerRotationTextObject.GetComponent<TextMesh> ();
 			characterPositionTextMesh = characterPositionTextObject.GetComponent<TextMesh> ();
 			characterRotationTextMesh = characterRotationTextObject.GetComponent<TextMesh> ();
+			pointerPositionTextMesh = pointerPositionTextObject.GetComponent<TextMesh> ();
 		}
 		
 		#endregion // UNTIY_MONOBEHAVIOUR_METHODS
@@ -56,21 +60,21 @@ namespace Vuforia
 		{
 			Vector3 controllerObjectPosition = controllerObject.transform.position;
 			Vector3 controllerObjectRotation = controllerObject.transform.eulerAngles;
+			Vector3 pointerDirection = directionPointer.transform.position - controllerObjectPosition;
 			controllerPositionTextMesh.text = "Controller Position: " + controllerObjectPosition; 
-			controllerRotationTextMesh.text = "Controller Rotation: " + controllerObjectRotation;
+			controllerRotationTextMesh.text = "Pointer Positon: " + directionPointer.transform.position;
 			Vector3 characterPosition = character.transform.position;
 			Vector3 characterRotation = character.transform.eulerAngles;
-			characterPositionTextMesh.text = "Character Position: " + characterPosition; 
+			characterPositionTextMesh.text = "Pointer direction: " + pointerDirection; 
 			characterRotationTextMesh.text = "Character Rotation: " + characterRotation;
 
-			Vector3 moveDirection = new Vector3 (0f, 0f, 0.1f);
+			//Vector3 moveDirection = new Vector3 (0f, 0f, 0.1f);
+			Vector3 moveDirection = new Vector3(pointerDirection.x, 0, pointerDirection.z); // only take x and z components of direction
+			pointerPositionTextMesh.text = "Move Direction z: " + moveDirection.z;
 			if (isTracked && controllerObjectRotation.x > 30f && controllerObjectRotation.x < 330f) {
-				if (controllerObjectRotation.x > 180f) {
-					moveDirection *= -1f;
-				}
-				character.transform.Translate (moveDirection);
+				character.transform.Translate (moveDirection, Space.World);
 			}
-			Vector3 rotateDirection = new Vector3 (0f, 1f, 0f);
+			Vector3 rotateDirection = new Vector3 (0f, 0.3f, 0f);
 			if (isTracked && controllerObjectRotation.z > 30f && controllerObjectRotation.z < 330f) {
 				if (controllerObjectRotation.z < 180f) {
 					rotateDirection *= -1f;

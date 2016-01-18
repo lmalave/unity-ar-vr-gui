c1a2200297457e4390851e3523fd7e861f600276
lmalave <lmalave@hugeinc.com>
2016-01-18 17:58:13 -0500

    Added timing code so movement speeds don't vary based on framerate

--- Assets/Scripts/VirtualJoystickHandler.cs
+++ Assets/Scripts/VirtualJoystickHandler.cs
@@ -12,7 +12,8 @@ public class VirtualJoystickHandler : MonoBehaviour {
 	public GameObject pointerPositionTextObject;
 	public float TurnAngleThreshold = 10.0f;
 	public float TurnRateOffset = 0.0f;
-	public float MaxTurnRate = 1.5f;
+	public float MaxMovementSpeed = 2.0f;  // in meters/sec
+	public float MaxTurnRate = 50f; // in degrees/sec
 	public float TurnAngleToRateScale = 6.0f;	
 
 	private TextMesh controllerPositionTextMesh;
@@ -47,17 +48,18 @@ public class VirtualJoystickHandler : MonoBehaviour {
 		//Vector3 moveDirection = new Vector3 (0f, 0f, 0.1f);
 		Vector3 moveDirection = new Vector3 (Mathf.Pow(transform.up.x, 2)*Mathf.Sign(transform.up.x), 0, Mathf.Pow(transform.up.z, 2)*Mathf.Sign(transform.up.z)); // only take x and z components of direction
 
-		if (moveDirection.magnitude > 0.1f) {
-			character.transform.Translate (moveDirection*0.1f, Space.World);
+		if (moveDirection.magnitude > 0.1f) {			
+			character.transform.Translate (moveDirection*MaxMovementSpeed*Time.deltaTime, Space.World);
 		}
 		Vector3 controllerXZ = Vector3.ProjectOnPlane(transform.right, yAxis);
 		Vector3 characterXZ = Vector3.ProjectOnPlane (character.transform.right, yAxis);
 		Vector3 controllerForwardDirectionInCharacterSpace = 
 			character.transform.InverseTransformDirection (transform.forward);
 		float controllerRelativeYRotation = Vector3.Angle (controllerXZ, characterXZ);
-		pointerPositionTextMesh.text = "controllerForwardDirectionInCharacterSpace: " + controllerForwardDirectionInCharacterSpace;
 		if (Mathf.Abs(controllerForwardDirectionInCharacterSpace.x) > 0.1f) {
-			Vector3 rotateDirection = new Vector3 (0f, controllerForwardDirectionInCharacterSpace.x - 0.1f, 0f);
+			float amountToRotate = (controllerForwardDirectionInCharacterSpace.x - 0.1f) * MaxTurnRate * Time.deltaTime;
+			pointerPositionTextMesh.text = "amountToRotate: " + MaxTurnRate + ", " + Time.deltaTime + ", " + (MaxTurnRate * Time.deltaTime) + ", " +amountToRotate;
+			Vector3 rotateDirection = new Vector3 (0f, amountToRotate, 0f);
 			character.transform.Rotate (rotateDirection);
 		}
 	}

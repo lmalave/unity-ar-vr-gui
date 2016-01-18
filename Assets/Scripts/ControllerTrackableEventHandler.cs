cbf87e993d7752998757a3eee73c065e9a91092c
lmalave <lmalave@hugeinc.com>
2016-01-18 17:01:42 -0500

    Moving movement logic to VirtualJoystickHandler

--- Assets/Scripts/ControllerTrackableEventHandler.cs
+++ Assets/Scripts/ControllerTrackableEventHandler.cs
@@ -14,29 +14,12 @@ namespace Vuforia
 	{
 		#region PUBLIC_MEMBER_VARIABLES
 		public GameObject controllerObject;
-		public GameObject controllerPositionTextObject;
-		public GameObject controllerRotationTextObject;
-		public GameObject character;
-		public GameObject characterPositionTextObject;
-		public GameObject characterRotationTextObject;
-		public GameObject directionPointer;
-		public GameObject pointerPositionTextObject;
-		public float TurnAngleThreshold = 10.0f;
-		public float TurnRateOffset = 0.0f;
-		public float MaxTurnRate = 1.5f;
-		public float TurnAngleToRateScale = 6.0f;
 		#endregion // PUBLIC_MEMBER_VARIABLES
 
 		#region PRIVATE_MEMBER_VARIABLES
-		private TextMesh controllerPositionTextMesh;
-		private TextMesh controllerRotationTextMesh;
-		private TextMesh characterPositionTextMesh;
-		private TextMesh characterRotationTextMesh;
-		private TextMesh pointerPositionTextMesh;
 		private TrackableBehaviour mTrackableBehaviour;
 		private bool isTracked = false;
-		private static Vector3 yAxis = new Vector3 (0f, 1f, 0f);
-		
+
 		#endregion // PRIVATE_MEMBER_VARIABLES
 		
 		
@@ -51,46 +34,15 @@ namespace Vuforia
 				mTrackableBehaviour.RegisterTrackableEventHandler(this);
 			}
 
-			controllerPositionTextMesh = controllerPositionTextObject.GetComponent<TextMesh> ();
-			controllerRotationTextMesh = controllerRotationTextObject.GetComponent<TextMesh> ();
-			characterPositionTextMesh = characterPositionTextObject.GetComponent<TextMesh> ();
-			characterRotationTextMesh = characterRotationTextObject.GetComponent<TextMesh> ();
-			pointerPositionTextMesh = pointerPositionTextObject.GetComponent<TextMesh> ();
 		}
 		
 		#endregion // UNTIY_MONOBEHAVIOUR_METHODS
 		
 		void Update()
 		{
-			// TODO: move movement logic to a different script
-			// TODO: should add time sampling so it's not more sensitive for higher framerates
-			Vector3 controllerObjectPosition = controllerObject.transform.position;
-			Vector3 controllerObjectRotation = controllerObject.transform.eulerAngles;
-			Vector3 pointerDirection = directionPointer.transform.position - controllerObjectPosition;
-			pointerDirection.Normalize ();
-			controllerPositionTextMesh.text = "Controller Position: " + controllerObjectPosition; 
-			controllerRotationTextMesh.text = "Controller Rotation: " + controllerObjectRotation;
-			Vector3 characterPosition = character.transform.position;
-			Vector3 characterRotation = character.transform.eulerAngles;
-			characterPositionTextMesh.text = "Pointer direction: " + pointerDirection; 
-			characterRotationTextMesh.text = "Character Rotation: " + characterRotation;
 
-			//Vector3 moveDirection = new Vector3 (0f, 0f, 0.1f);
-			Vector3 moveDirection = new Vector3 (Mathf.Pow(pointerDirection.x, 2)*Mathf.Sign(pointerDirection.x), 0, Mathf.Pow(pointerDirection.z, 2)*Mathf.Sign(pointerDirection.z)); // only take x and z components of direction
 			if (isTracked) {
-				if (moveDirection.magnitude > 0.1f) {
-					//character.transform.Translate (moveDirection*0.1f, Space.World);
-				}
-				Vector3 controllerXZ = Vector3.ProjectOnPlane(controllerObject.transform.right, yAxis);
-				Vector3 characterXZ = Vector3.ProjectOnPlane (character.transform.right, yAxis);
-				Vector3 controllerForwardDirectionInCharacterSpace = 
-					character.transform.InverseTransformDirection (controllerObject.transform.forward);
-				float controllerRelativeYRotation = Vector3.Angle (controllerXZ, characterXZ);
-				pointerPositionTextMesh.text = "controllerForwardDirectionInCharacterSpace: " + controllerForwardDirectionInCharacterSpace;
-				if (Mathf.Abs(controllerForwardDirectionInCharacterSpace.x) > 0.1f) {
-					Vector3 rotateDirection = new Vector3 (0f, controllerForwardDirectionInCharacterSpace.x - 0.1f, 0f);
-					character.transform.Rotate (rotateDirection);
-				}
+				controllerObject.SendMessage ("HandleJoystickMovement");
 			}
 
 
---
+++ Assets/Scripts/VirtualJoystickHandler.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VirtualJoystickHandler : MonoBehaviour {
+
+	public GameObject controllerPositionTextObject;
+	public GameObject controllerRotationTextObject;
+	public GameObject character;
+	public GameObject characterPositionTextObject;
+	public GameObject characterRotationTextObject;
+	public GameObject directionPointer;
+	public GameObject pointerPositionTextObject;
+	public float TurnAngleThreshold = 10.0f;
+	public float TurnRateOffset = 0.0f;
+	public float MaxTurnRate = 1.5f;
+	public float TurnAngleToRateScale = 6.0f;	
+
+	private TextMesh controllerPositionTextMesh;
+	private TextMesh controllerRotationTextMesh;
+	private TextMesh characterPositionTextMesh;
+	private TextMesh characterRotationTextMesh;
+	private TextMesh pointerPositionTextMesh;
+
+	private static Vector3 yAxis = new Vector3 (0f, 1f, 0f);
+
+	// Use this for initialization
+	void Start () {
+		controllerPositionTextMesh = controllerPositionTextObject.GetComponent<TextMesh> ();
+		controllerRotationTextMesh = controllerRotationTextObject.GetComponent<TextMesh> ();
+		characterPositionTextMesh = characterPositionTextObject.GetComponent<TextMesh> ();
+		characterRotationTextMesh = characterRotationTextObject.GetComponent<TextMesh> ();
+		pointerPositionTextMesh = pointerPositionTextObject.GetComponent<TextMesh> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void HandleJoystickMovement() {
+		//pointerDirection.Normalize ();
+		controllerPositionTextMesh.text = "Controller Position: " + transform.position; 
+		controllerRotationTextMesh.text = "Controller Rotation: " + transform.eulerAngles;
+		characterPositionTextMesh.text = "Pointer direction: " + transform.up; 
+		characterRotationTextMesh.text = "Character Rotation: " + character.transform.eulerAngles;
+
+		//Vector3 moveDirection = new Vector3 (0f, 0f, 0.1f);
+		Vector3 moveDirection = new Vector3 (Mathf.Pow(transform.up.x, 2)*Mathf.Sign(transform.up.x), 0, Mathf.Pow(transform.up.z, 2)*Mathf.Sign(transform.up.z)); // only take x and z components of direction
+
+		if (moveDirection.magnitude > 0.1f) {
+			character.transform.Translate (moveDirection*0.1f, Space.World);
+		}
+		Vector3 controllerXZ = Vector3.ProjectOnPlane(transform.right, yAxis);
+		Vector3 characterXZ = Vector3.ProjectOnPlane (character.transform.right, yAxis);
+		Vector3 controllerForwardDirectionInCharacterSpace = 
+			character.transform.InverseTransformDirection (transform.forward);
+		float controllerRelativeYRotation = Vector3.Angle (controllerXZ, characterXZ);
+		pointerPositionTextMesh.text = "controllerForwardDirectionInCharacterSpace: " + controllerForwardDirectionInCharacterSpace;
+		if (Mathf.Abs(controllerForwardDirectionInCharacterSpace.x) > 0.1f) {
+			Vector3 rotateDirection = new Vector3 (0f, controllerForwardDirectionInCharacterSpace.x - 0.1f, 0f);
+			character.transform.Rotate (rotateDirection);
+		}
+	}
+}
